eccde9ed67b074564ac63fe9d78d8815c3bf3f85
Kristina Capisrano <kristina.gomezcapistrano@gmail.com>
2019-04-25 21:33:20 -0700

    updated GUI

--- src/MojoTransportMain.java
+++ src/MojoTransportMain.java
@@ -53,37 +53,38 @@ public class MojoTransportMain extends JFrame {
 		JFrame f=new JFrame("Basic Query UI"); 
 					//submit button
 		JButton b=new JButton("Submit");    
-		b.setBounds(150,100,140, 30);
+		b.setBounds(418,78,140, 30);
 		JButton b2=new JButton("Submit");    
-		b2.setBounds(150,200,140, 30);
+		b2.setBounds(418,164,140, 30);
 		JButton b3=new JButton("Show Ground Companies");    
-		b3.setBounds(350,100,200, 30);
+		b3.setBounds(20,10,200, 30);
 					//enter name label
 		JLabel label = new JLabel();		
 		label.setText("Enter Model to find Vehicle");
-		label.setBounds(0, 10, 150, 100);
+		label.setBounds(177, 52, 184, 29);
 		JLabel label2 = new JLabel();
 		label2.setText("Enter Vehicle for Purpose");
-		label2.setBounds(0, 100, 150, 100);
+		label2.setBounds(177, 130, 184, 39);
 					//empty label which will show event after button clicked
 		JTextArea label1 = new JTextArea();
-		label1.setBounds(100, 300, 400, 200);
+		label1.setBackground(UIManager.getColor("Button.background"));
+		label1.setBounds(104, 232, 424, 225);
 					//textfield to enter name
 		JTextField textfield= new JTextField();
-		textfield.setBounds(154, 50, 130, 30);
+		textfield.setBounds(154, 77, 230, 30);
 		JTextField textfield2= new JTextField();
-		textfield2.setBounds(154, 150, 130, 30);
+		textfield2.setBounds(154, 163, 230, 30);
 					//add to frame
-		f.add(label1);
-		f.add(label2);
-		f.add(textfield);
-		f.add(textfield2);
-		f.add(label);
-		f.add(b);
-		f.add(b2);
-		f.add(b3);
+		f.getContentPane().add(label1);
+		f.getContentPane().add(label2);
+		f.getContentPane().add(textfield);
+		f.getContentPane().add(textfield2);
+		f.getContentPane().add(label);
+		f.getContentPane().add(b);
+		f.getContentPane().add(b2);
+		f.getContentPane().add(b3);
 		f.setSize(600,600);    
-		f.setLayout(null);    
+		f.getContentPane().setLayout(null);    
 		f.setVisible(true);    
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.addWindowListener(new WindowAdapter()
